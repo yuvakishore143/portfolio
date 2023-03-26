ef4c33e3e5024a61af30372a6236b73f1b550199
yuva kishore <vishwanathyuva143@gmail.com>
2023-03-26 21:31:35 +0530

    added footer

--- src/components/particles/Footer.js
+++ src/components/particles/Footer.js
@@ -1,7 +1,38 @@
 import "../../styles/Footer.css";
+import {AiFillGithub , AiFillLinkedin , AiFillInstagram} from 'react-icons/ai'
 
 const Footer = () => {
-  return <div className="footer-cont"></div>;
+  return (
+    <div className="footer-cont d-flex align-items-center ">
+      <ul className="footer-min-cont d-flex justify-content-around">
+        <li className=" footer-list d-flex flex-row justify-content-center align-items-center">
+          <div className="footer-logo-cont ">
+            <AiFillGithub className="footer-logo"/>
+          </div>
+          <div className="footer-text-cont">
+            <a className="footer-text-link" target="_blank" href="https://github.com/yuvakishore143">yuvakishore143</a>
+          </div>
+        </li>
+        <li className="footer-list d-flex flex-row justify-content-center align-items-center">
+          <div className="footer-logo-cont">
+            <AiFillLinkedin className="footer-logo"/>
+          </div>
+          <div className="footer-text-cont">
+            <a className="footer-text-link" target="_blank" href="https://www.linkedin.com/in/yuva-kishore-vishwanath/">yuvakishorevishwanath</a>
+          </div>
+        </li>
+        <li className=" footer-list d-flex flex-row justify-content-center align-items-center">
+          <div className="footer-logo-cont">
+            <AiFillInstagram className="footer-logo"/>
+          </div>
+          <div className="footer-text-cont">
+            <a className="footer-text-link" target="_blank" href="https://www.instagram.com/yuvavishwanath/">@yuvavishwanath</a>
+          </div>
+        </li>
+
+      </ul>
+    </div>
+  );
 };
 
 export default Footer;
